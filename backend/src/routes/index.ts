21ad02fad5f160e7872c7d69584fc0e707af668c
Daniil <kashel369@bk.ru>
2023-04-06 16:36:26 +0300

    add cors + crush test + change front api url

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,26 +1,27 @@
-import {
-  Router, Request, Response, NextFunction,
-} from 'express';
-import userRouter from './users';
-import cardRouter from './cards';
-import auth from '../middlewares/auth';
-import NotFoundError from '../errors/not-found-error';
-import {
-  createUser, login,
-} from '../controllers/users';
-import { validateUserBody, validateAuthentication } from '../middlewares/validatons';
+import { NextFunction, Request, Response, Router } from 'express'
+import { createUser, login } from '../controllers/users'
+import NotFoundError from '../errors/not-found-error'
+import auth from '../middlewares/auth'
+import { validateAuthentication, validateUserBody } from '../middlewares/validatons'
+import cardRouter from './cards'
+import userRouter from './users'
 
-const router = Router();
-router.post('/signup', validateUserBody, createUser);
-router.post('/signin', validateAuthentication, login);
+const router = Router()
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт')
+  }, 0)
+})
+router.post('/signup', validateUserBody, createUser)
+router.post('/signin', validateAuthentication, login)
 
 // все роуты, кроме /signin и /signup, защищены авторизацией;
-router.use(auth);
-router.use('/users', userRouter);
-router.use('/cards', cardRouter);
+router.use(auth)
+router.use('/users', userRouter)
+router.use('/cards', cardRouter)
 
 router.use((req: Request, res: Response, next: NextFunction) => {
-  next(new NotFoundError('Маршрут не найден'));
-});
+  next(new NotFoundError('Маршрут не найден'))
+})
 
-export default router;
+export default router

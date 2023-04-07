b5f95af75af8863317dfc42c2a42d4e3b70fec77
Daniil <kashel369@bk.ru>
2023-04-07 03:26:47 +0300

    change predeploy to predeploy local

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -19,7 +19,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
+      'pre-deploy-local': `scp ./.env ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': 'cd ~/mesto-backend/source/backend/ && npm i && npm run build && pm2 start',
     },
   },
